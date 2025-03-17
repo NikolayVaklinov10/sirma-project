ad58d0c4a18dec333fe65b597a0c1acca20985ee
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2025-03-17 13:06:17 -0700

    the employee pair with no argument constructor

--- src/main/java/com/sirma/Nikolay_Vaklinov/model/EmployeePair.java
+++ src/main/java/com/sirma/Nikolay_Vaklinov/model/EmployeePair.java
@@ -2,13 +2,19 @@ package com.sirma.Nikolay_Vaklinov.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
 @Data
-@AllArgsConstructor
+@NoArgsConstructor 
 public class EmployeePair {
 
     private Long employeeId1;
     private Long employeeId2;
     private Long projectId;
     private Long daysWorked;
+
+    public EmployeePair(Long employeeId, Long employeeId1, Long projectId, long daysWorked) {
+    }
 }
