845f653088e19e1e7b18a6a416a402cda2784ba5
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2025-03-17 09:31:37 -0700

    the main file

---
+++ src/main/java/com/sirma/Nikolay_Vaklinov/NikolayVaklinovEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.sirma.Nikolay_Vaklinov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikolayVaklinovEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikolayVaklinovEmployeesApplication.class, args);
+	}
+
+}
