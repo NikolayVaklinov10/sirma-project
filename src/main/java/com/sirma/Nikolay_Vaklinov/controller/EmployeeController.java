042c85d571dbba317d2ea9256c17abb184ffc879
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2025-03-17 10:46:49 -0700

    the uploading method

--- src/main/java/com/sirma/Nikolay_Vaklinov/controller/EmployeeController.java
+++ src/main/java/com/sirma/Nikolay_Vaklinov/controller/EmployeeController.java
@@ -1,7 +1,54 @@
 package com.sirma.Nikolay_Vaklinov.controller;
 
+import com.sirma.Nikolay_Vaklinov.model.Employee;
+import com.sirma.Nikolay_Vaklinov.model.EmployeePair;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class EmployeeController {
+
+    // the method is dealing with uploading and parsing
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, Model model) {
+        List<EmployeePair> pairs = new ArrayList<>();
+        try {
+            Reader reader = new InputStreamReader(file.getInputStream());
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader().parse(reader);
+
+            List<Employee> employees = new ArrayList<>();
+            for (CSVRecord record : records) {
+                Employee employee = new Employee();
+                employee.setEmployeeId(Long.parseLong(record.get("EmployeeID")));
+                employee.setProjectId(Long.parseLong(record.get("ProjectID")));
+                employee.setStartDate(LocalDate.parse(record.get("StartDate")));
+                String dateTo = record.get("DateTo");
+                employee.setEndDate(dateTo.equals("NULL") ? LocalDate.now() : LocalDate.parse(dateTo));
+
+                employees.add(employee);
+            }
+
+            pairs = getEmployeePairs(employees);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("pairs", pairs);
+        return "index"; // Thymeleaf template to display data
+    }
+
+
 }
