8149c1cf9b4491abea6a32d6d16b37a9ea871ecb
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2025-03-17 09:38:45 -0700

    the short Employee class without getters and setters

--- src/main/java/com/sirma/Nikolay_Vaklinov/model/Employee.java
+++ src/main/java/com/sirma/Nikolay_Vaklinov/model/Employee.java
@@ -1,4 +1,13 @@
 package com.sirma.Nikolay_Vaklinov.model;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class Employee {
+    private Long employeeId;
+    private Long projectId;
+    private LocalDate startDate;
+    private LocalDate endDate;
 }
